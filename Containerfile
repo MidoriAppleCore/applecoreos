4a3eacc558b5be14bd159087b3d7a6c021de5404
MidoriAppleCore <MidoriAppleCore@Gmail.com>
2024-07-13 05:39:36 -0600

    correctly configuring build script for rpm ostree stuff, silly typo

--- Containerfile
+++ Containerfile
@@ -52,10 +52,7 @@ FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${SOURCE_TAG}
 COPY build.sh /tmp/build.sh
 COPY wallpaper.jpg /tmp/wallpaper.jpg
 
-RUN mkdir -p /var/lib/alternatives && \
-    /tmp/build.sh && \
-    ostree container commit
-## NOTES:
-# - /var/lib/alternatives is required to prevent failure with some RPM installs
-# - All RUN commands must end with ostree container commit
-#   see: https://coreos.github.io/rpm-ostree/container/#using-ostree-container-commit
+RUN chmod +x /tmp/build.sh && /tmp/build.sh
+
+# Install Firefox via Flatpak (separate RUN to avoid core dump issue)
+RUN flatpak install -y flathub org.mozilla.firefox
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 set -ouex pipefail
@@ -6,39 +5,30 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 rpm-ostree install -y tmux podman podman-compose docker curl wget git neovim \
                lxde-common lxterminal lightdm conman virt-manager distrobox \
                flatpak
 rpm-ostree override remove firefox firefox-langpacks
 
+### Configure Flatpak
+# Add the Flathub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-# Install Firefox via Flatpak
-flatpak install -y flathub org.mozilla.firefox
 
+# Enable necessary services
 systemctl enable podman.socket
 systemctl enable lightdm
 systemctl enable flatpak-system-helper
 
 ### Add user 'midori' with no password and add to necessary groups
-# Create the user with no password
 useradd -m -G wheel,docker -s /bin/bash midori
 passwd -d midori
-# Ensure user has access to necessary groups
 usermod -aG podman midori
 
 ### Set LXDE default configurations
-# Copy the wallpaper to the appropriate directory
 mkdir -p /usr/share/backgrounds
 cp /tmp/wallpaper.jpg /usr/share/backgrounds/default_wallpaper.jpg
-# Create the default LXDE configuration directory if it doesn't exist
+
 mkdir -p /etc/xdg/lxsession/LXDE
-# Set the default wallpaper
 cat <<EOF > /etc/xdg/pcmanfm/LXDE/desktop-items-0.conf
 [*]
 wallpaper_mode=zoom
@@ -46,3 +36,15 @@ wallpaper_common=1
 wallpaper=/usr/share/backgrounds/default_wallpaper.jpg
 EOF
 
+cat <<EOF > /etc/xdg/lxsession/LXDE/autostart
+@lxpanel --profile LXDE
+@pcmanfm --desktop --profile LXDE
+EOF
+
+### Cleanup
+
+# Remove unnecessary packages
+rpm-ostree cleanup -m
+
+# Remove temporary files and caches
+rm -rf /var/cache/dnf /var/lib/dnf /tmp/* /var/tmp/*
