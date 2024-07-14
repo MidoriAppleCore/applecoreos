7f503b7962c8da509ab04f30c9e485ac6787b966
MidoriAppleCore <MidoriAppleCore@Gmail.com>
2024-07-13 20:00:25 -0600

    changing installed by default programs.

--- build.sh
+++ build.sh
@@ -6,16 +6,17 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 ### Install packages
-rpm-ostree install -y tmux podman podman-compose docker curl wget git neovim \
-               lxde-common lxterminal lightdm conman virt-manager distrobox \
-               flatpak
+rpm-ostree install -y tmux podman podman-compose curl wget git neovim \
+               lxde-common alacritty lightdm NetworkManager virt-manager distrobox \
+               flatpak xorg-x11-xdm obconf xarchiver feh mpv htop xpdf xclip \
+
 
 #remove default firefox since it might force us to update the base system more often than we want to because of exploits etc
 rpm-ostree override remove firefox firefox-langpacks
 
 # Enable necessary services
 systemctl enable podman.socket
-systemctl enable lightdm
+systemctl enable xdm
 systemctl enable flatpak-system-helper
 
 ### Set LXDE default configurations
@@ -23,11 +24,27 @@ mkdir -p /usr/share/backgrounds
 cp /tmp/wallpaper.jpg /usr/share/backgrounds/default_wallpaper.jpg
 
 mkdir -p /etc/xdg/lxsession/LXDE
-cat <<EOF > /etc/xdg/pcmanfm/LXDE/desktop-items-0.conf
+cat <<EOF > /etc/xdg/pcmanfm/LXDE/pcmanfm.conf
 [*]
-wallpaper_mode=zoom
-wallpaper_common=1
+[config]
+bm_open_method=0
+su_cmd=xdg-su -c '%s'
+
+[desktop]
+wallpaper_mode=crop
 wallpaper=/usr/share/backgrounds/default_wallpaper.jpg
+desktop_bg=#000000
+desktop_fg=#ffffff
+desktop_shadow=#000000
+
+[ui]
+always_show_tabs=0
+hide_close_btn=0
+win_width=640
+win_height=480
+view_mode=icon
+show_hidden=0
+sort=name;ascending;
 EOF
 
 ### Cleanup
