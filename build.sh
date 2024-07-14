1fdfdf84337a88a54342281d830d6ef79c97239e
MidoriAppleCore <MidoriAppleCore@Gmail.com>
2024-07-13 21:24:10 -0600

    changing installed by default programs.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 rpm-ostree install -y tmux podman podman-compose curl wget git neovim \
                lxde-common lxterminal NetworkManager virt-manager distrobox \
-               flatpak obconf xarchiver feh htop xpdf xclip w3m\ lightdm \
+               flatpak obconf xarchiver feh htop xpdf xclip w3m lightdm \
                lxinput lxrandr lxsession-edit lxsession lxappearance 
 
 
