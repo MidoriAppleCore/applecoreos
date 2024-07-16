86b689b7c4afc47e2b160a401861fb51d06e1f5e
MidoriAppleCore <MidoriAppleCore@Gmail.com>
2024-07-16 06:51:50 -0600

    changing dm test

--- gnome_build.sh
+++ gnome_build.sh
@@ -12,11 +12,12 @@ RELEASE="$(rpm -E %fedora)"
 #               lxinput lxrandr lxsession-edit lxsession lxappearance \
 #               pop-icon-theme sshfs
 rpm-ostree install -y tmux podman podman-compose curl wget git vim-minimal \
-               distrobox \
-               htop w3m \
-               sshfs \
-               lightdm \
-               labwc \ 
+               virt-manager distrobox \
+               flatpak htop w3m gdm \
+               pop-icon-theme sshfs \
+               gnome-shell gnome-terminal \
+
+
 
 #remove default firefox since it might force us to update the base system more often than we want to because of exploits etc
 rpm-ostree override remove firefox firefox-langpacks
@@ -24,7 +25,7 @@ rpm-ostree override remove firefox firefox-langpacks
 # Enable necessary services
 systemctl enable podman.socket
 systemctl enable flatpak-system-helper
-systemctl enable lightdm
+systemctl enable gdm
 
 ### Cleanup
 
