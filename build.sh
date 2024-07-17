1b6a34473b85355b57f80cd30f92e6ef1cd86d8e
MidoriAppleCore <MidoriAppleCore@Gmail.com>
2024-07-16 23:17:59 -0600

    changing dm test

--- build.sh
+++ build.sh
@@ -15,7 +15,8 @@ rpm-ostree install -y tmux podman podman-compose curl wget git vim-minimal \
                virt-manager distrobox \
                flatpak htop w3m gdm \
                pop-icon-theme sshfs \
-               gnome-flashback gnome-terminal \
+               gnome-classic-session gnome-terminal \
+               nautilus \
 
 
 
