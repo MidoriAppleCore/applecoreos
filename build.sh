d11f6ab716d709780c3d81a15494b1dfd8a8c536
MidoriAppleCore <rosejQR@gmail.com>
2024-07-19 04:40:50 -0400

    replace guake with alacritty, sound mixer

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ rpm-ostree install -y tmux podman podman-compose curl wget git neovim leafpad \
                flatpak obconf xarchiver gpicview htop xpdf xclip w3m lightdm \
                lxinput lxrandr lxterminal lxsession-edit lxsession lxappearance \
                pop-icon-theme sshfs gnome-screenshot pipewire alsa-utils \
-               terminus* lxpolkit ansible guake
+               terminus* lxpolkit ansible alacritty pavucontrol
 
 #remove default firefox since it might force us to update the base system more often than we want to because of exploits etc
 rpm-ostree override remove firefox firefox-langpacks
