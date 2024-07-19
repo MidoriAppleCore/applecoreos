05d67d5712f07d14ccb4d3baf3555d99b3653d6c
MidoriAppleCore <rosejQR@gmail.com>
2024-07-18 20:57:38 -0400

    gnome screenshot added

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ rpm-ostree install -y tmux podman podman-compose curl wget git neovim leafpad \
                lxde-common lxterminal NetworkManager virt-manager distrobox \
                flatpak obconf xarchiver gpicview htop xpdf xclip w3m lightdm \
                lxinput lxrandr lxsession-edit lxsession lxappearance \
-               pop-icon-theme sshfs
+               pop-icon-theme sshfs gnome-screenshot
 
 #remove default firefox since it might force us to update the base system more often than we want to because of exploits etc
 rpm-ostree override remove firefox firefox-langpacks
